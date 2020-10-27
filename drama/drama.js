ec3312759e108b4f797a9fc793fafac1f24e9270
jufiris <dropsleutel@live.nl>
2020-10-27 13:44:01 +0100

    Update drama.js

--- drama/drama.js
+++ drama/drama.js
@@ -20,3 +20,19 @@
     });
   }, 2500);
 })();
+
+$(document).ready(function() {
+  $(window).on("load scroll resize", function() {
+    var docHeight = $(document).height();
+    var windowPos = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    var completion = windowPos / (docHeight - windowHeight);
+
+    if (docHeight <= windowHeight) {
+      $("#progress").width(windowWidth);
+    } else {
+      $("#progress").width(completion * windowWidth);
+    }
+  });
+});
