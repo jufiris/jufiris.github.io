5898cddf9ee5db189f3921e1e1bbba688146eb6d
jufiris <dropsleutel@live.nl>
2020-11-25 22:55:39 +0100

    Update drama.js

--- drama/drama.js
+++ drama/drama.js
@@ -1,46 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {});
 
-});
-
-$('.carousel').carousel({
-  interval: false,
+$(".carousel").carousel({
+	interval: false
 });
 
 (function () {
-  var words = [
-      "loslaten",
-      "ontdekken",
-      "expressie",
-      "zelfvertrouwen",
-      "impulsief",
-      "luisteren",
-      "bewustworden",
-      "vakoverschreidend",
-      "elkaar leren kennen",
-      "leuk",
-    ],
-    i = 0;
-  setInterval(function () {
-    $("#changingword").fadeOut(function () {
-      $(this)
-        .html(words[(i = (i + 1) % words.length)])
-        .fadeIn();
-    });
-  }, 2500);
+	var words = [
+			"loslaten",
+			"ontdekken",
+			"expressie",
+			"zelfvertrouwen",
+			"impulsief",
+			"luisteren",
+			"bewustworden",
+			"vakoverschreidend",
+			"elkaar leren kennen",
+			"leuk"
+		],
+		i = 0;
+	setInterval(function () {
+		$("#changingword").fadeOut(function () {
+			$(this)
+				.html(words[i % words.length])
+				.fadeIn("slow");
+			i++;
+		});
+	}, 2500);
 })();
 
-$(document).ready(function() {
-  $(window).on("load scroll resize", function() {
-    var docHeight = $(document).height();
-    var windowPos = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    var windowWidth = $(window).width();
-    var completion = windowPos / (docHeight - windowHeight);
+$(document).ready(function () {
+	$(window).on("load scroll resize", function () {
+		var docHeight = $(document).height();
+		var windowPos = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var windowWidth = $(window).width();
+		var completion = windowPos / (docHeight - windowHeight);
 
-    if (docHeight <= windowHeight) {
-      $("#progress").width(windowWidth);
-    } else {
-      $("#progress").width(completion * windowWidth);
-    }
-  });
+		if (docHeight <= windowHeight) {
+			$("#progress").width(windowWidth);
+		} else {
+			$("#progress").width(completion * windowWidth);
+		}
+	});
 });
